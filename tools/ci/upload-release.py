cdf2ab522f0ce541ea1cc00362307d0455042501
Maxim Prokhorov <prokhorov.max@outlook.com>
2021-05-12 08:53:40 +0300

    ci: release single .zip, merge all available build artifacts

--- tools/ci/upload-release.py
+++ tools/ci/upload-release.py
@@ -1,6 +1,8 @@
+import sys
 import os
 import pathlib
 from itertools import chain
+from zipfile import ZipFile
 from github import Github
 
 
@@ -12,24 +14,35 @@ def find_zip(directory):
     return pathlib.Path(directory).glob("**/*.zip")
 
 
+def merge_zip(target, sources):
+    with ZipFile(target, "a") as target_archive:
+        for source in sources:
+            with ZipFile(source, "r") as source_archive:
+                for name in source_archive.namelist():
+                    # note that .zip format allows the name to be specified multiple times
+                    # only write unique names into the target archive, ignoring duplicates
+                    try:
+                        target_archive.getinfo(name)
+                    except KeyError:
+                        target_archive.writestr(name, source_archive.open(name).read())
+
+
 if __name__ == "__main__":
-    # Binaries/ - .elf + .bin
-    # artifact/ - docs, upload tools, etc.
-    archives = [
-        str(zip_file) for zip_file in chain(find_zip("Binaries"), find_zip("artifact"))
-    ]
-    if not archives:
+    # artifacts/Binaries/ - .elf + .bin
+    # artifacts/artifact/ - docs, upload tools, etc.
+    sources = [str(zip_file) for zip_file in find_zip(sys.argv[1])]
+    if not sources:
         raise NoZipFilesFound
 
     tag = os.environ["GITHUB_REF"][len("refs/tags/") :]
     message = os.environ.get("RELEASE_NOTES", tag)
 
-    print("Prepared for tag={} and archives={}".format(tag, archives))
-
     gh = Github(os.environ["GITHUB_TOKEN"])
     repo = gh.get_repo(os.environ["GITHUB_REPOSITORY"])
 
-    release = repo.create_git_release(tag=tag, name=tag, message=message)
+    target = "ESPEasy_{}.zip".format(tag)
+    print("preparing target={} from {} sources".format(target, len(sources)))
+    merge_zip(target, sources)
 
-    for path in archives:
-        release.upload_asset(path)
+    release = repo.create_git_release(tag=tag, name=tag, message=message)
+    release.upload_asset(target)
